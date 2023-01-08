f8503c9f12560c807a51d444ac5e9af325db4cc9
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2023-01-08 21:31:13 +0800

    feat:文件读写

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
 /*
  * 计算平均数
@@ -384,8 +385,21 @@ void fib() {
     printf("\n");
 }
 
+// 斐波那契数列2
+void fib_arr() {
+    long f[20] = {1, 1};
+    for (int i = 2; i < 20; ++i) {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+    for (int i = 0; i < 20; ++i) {
+        printf("%ld ", f[i]);
+    }
 
-// 斐波那契数列2:递归
+    printf("\n");
+}
+
+
+// 斐波那契数列3:递归
 long fib_recur(int n) {
     if (n <= 1) {
         return 1;
@@ -425,10 +439,201 @@ void test_prime_number() {
     }
 }
 
-// main 函数
-int main() {
-    test_prime_number();
+
+int max(int x, int y) {
+    return x > y ? x : y;
 }
+
+// 函数指针
+void test_fun_ptr() {
+    /* p 是函数指针 */
+    int (*p)(int, int) = &max; // &可以省略
+    int a, b, c, d;
+
+    printf("请输入三个数字:");
+    scanf("%d %d %d", &a, &b, &c);
+
+    /* 与直接调用函数等价，d = max(max(a, b), c) */
+    d = p(p(a, b), c);
+
+    printf("最大的数字是: %d\n", d);
+}
+
+// 字符串操作
+void test_str() {
+    char str1[14] = "runoob";
+    char str2[14] = "google";
+    char str3[14];
+    int len;
+
+    printf("str1 :  %s\n", str1);
+    printf("str2 :  %s\n", str2);
+    printf("str3 :  %s\n", str3);
+
+    /* 复制 str1 到 str3 */
+    strcpy(str3, str1);
+    printf("strcpy( str3, str1) , str3:  %s\n", str3);
+
+    /* 连接 str1 和 str2 */
+    strcat(str1, str2);
+    printf("strcat( str1, str2) , str1:  %s\n", str1);
+
+    /* 连接后，str1 的总长度 */
+    len = strlen(str1);
+    printf("strlen(str1) :  %d\n", len);
+}
+
+//编写一个程序，由用户输入一个任意字符串（长度小于 50），实现将该字符串反序，并将原字符串与反序后的字符串拼接在一起（原字符串放在前面，反序的字符串放在后面），在屏幕上输出拼接好的字符串。
+//比如：如果输入字符串：abcde，则输出字符串：abcdeedcba
+
+//求字符串长度
+int chptr_strlen(char *str) {
+    int count = 0;
+    while (*str != '\0') {
+        count++;
+        str++;  //指向下一指针
+    }
+    return count;
+}
+
+// 交换 str （i,j）位置的字符
+void swap_char(char *str, int i, int j) {
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+}
+
+// 逆序字符串
+void reverse_string(char *str) {
+    int left = 0;
+    int right = chptr_strlen(str) - 1;
+    while (left < right) {
+        swap_char(str, left++, right--);
+    }
+}
+
+// 镜像字符串
+void mirror_str() {
+    char input_str[100];
+    char output_str[200];
+
+    printf("请输入一个字符串:\n");
+    gets(input_str); // 读入字符串，赋值input_str
+    // strcpy到 output_str
+    strcpy(output_str, input_str);
+    // 逆序 input_str
+    reverse_string(input_str);
+    // append 逆序之后的 input_str 到 output_str
+    strcat(output_str, input_str);
+    // 输出 output_str
+    printf("镜像字符串输出:\n");
+    puts(output_str); // 用 puts 输出字符串也可以的
+}
+
+// 文件读写
+// 编写一个程序，从文本文件 string.txt 中逐个读入字符，组成字符串。
+//统计该字符串中字母（包括大写和小写字母）、数字和空格的个数，并将统计结果输出在屏幕上。假设文本文件 string.txt 存放在 D 盘根目录下，字符总数不超过 100，且可能含多行。
+//例如 string.txt 中的内容为： /Users/bytedance/CLionProjects/untitled/data/string.txt
+//Hello 50+ SJTUers,
+//Welcome to the Class CS0900.
+//则统计结果输出为：
+//字母数：31
+//数字数：6
+//空格数：6
+
+int is_letter(char c) {
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int is_number(char c) {
+    if (c >= '0' && c <= '9') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int is_space(char c) {
+    if (c == ' ') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int test_file_read() {
+    FILE *f;
+    f = fopen("/Users/bytedance/CLionProjects/untitled/data/string.txt", "r"); // 读文件的模式 r，这地方需要写对。w,w+,a+
+    if (NULL == f) {
+        printf("error!");
+        return 1;
+    }
+
+    // 把文件 /Users/bytedance/CLionProjects/untitled/data/string.txt 中的内容，填充到字符数组 str[100]
+    char ch;
+    char str[100];
+    int k = 0;
+    while ((ch = fgetc(f)) != EOF) {
+        str[k++] = ch;
+    }
+    // 按照惯用法，默认填充 '\0'
+    str[k] = '\0';
+
+    // 统计字母、数字、空格数
+    int letter_count = 0, number_count = 0, space_count = 0;
+    for (int j = 0; j < strlen(str); ++j) {
+        char c = str[j];
+        if (is_letter(c)) {
+            letter_count++;
+        } else if (is_number(c)) {
+            number_count++;
+        } else if (is_space(c)) {
+            space_count++;
+        }
+    }
+
+    printf("字母个数: %d\n数字个数: %d\n空格个数: %d\n", letter_count, number_count, space_count);
+    //字母个数: 31
+    //数字个数: 6
+    //空格个数: 6
+
+    // 关闭文件
+    fclose(f);
+
+    return 0;
+}
+
+// 写文件
+int test_file_write() {
+    FILE *fp;
+    // 打开文件，append写模式
+    fp = fopen("/Users/bytedance/CLionProjects/untitled/data/string.txt", "a"); // w：write, a：append
+
+    char str[] = "\nThis is Dream IT";
+    // 把 str 写入 fp 中
+    fwrite(str, sizeof(str), 1, fp);
+
+    fclose(fp);
+
+    return 0;
+}
+
+int main() {
+//    fib_recur(10);
+//    fib_arr();
+//    test_fun_ptr();
+//    mirror_str();
+//    test_prime_number();
+//    test_file_read();
+//    test_file_write();
+
+    return 0;
+}
+
 
 
 
